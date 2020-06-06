e983464016a85a95e44e04f74bbbfd3cae3a21db
IAmSmlBoy <iamsmolboy>
2020-06-06 15:20:43 +0800

    -

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 const achievements=document.getElementsByClassName('achievements')
+const Me=document.getElementsByClassName('AboutMe')
 
-function Achievements(){
-    for(i=1;i<achievements.length;i++){
-        if(achievements[i-1].style.display==='none'){
-            achievements[i-1].style.display='block'
+function AboutMe(){
+    for(i=1;i<Me.length+1;i++){
+        console.log(Me[i-1].style.display)
+        if(Me[i-1].style.display==='none'){
+            Me[i-1].style.display='block'
         }
         else{
-            achievements[i-1].style.display='none'
+            Me[i-1].style.display='none'
         }
     }
+}
+function Achievements(){
+    
 }
