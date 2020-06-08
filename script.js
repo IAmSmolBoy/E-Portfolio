0a2f8c369350269e23e0b695b6c9621bba324916
IAmSmlBoy <iamsmolboy>
2020-06-08 20:23:30 +0800

    Fixed animations

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const achievements=document.getElementsByClassName('achievements')
+const achievements=document.getElementById('achievements')
 const Me=document.getElementsByClassName('AboutMe')
 const Content=document.getElementById("content")
+const Myself=document.getElementById('Myself')
 
 
 
@@ -13,12 +14,15 @@ function AboutMe(){
     for(i=1;i<=Me.length;i++){
         if(Me[i-1].style.display==='none'){
             Me[i-1].style.display='block'
+            achievements.style.animation='Achievements 1s linear 1'
+            Myself.style.animation='Myself 1s linear 1'
         }
         else{
             Me[i-1].style.display='none'
         }
     }
 }
+
 
 
 
