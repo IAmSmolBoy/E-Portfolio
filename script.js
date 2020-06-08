2e1cebee5f4209e35f49ff67c1e96d2b59d5602b
IAmSmlBoy <iamsmolboy>
2020-06-08 20:43:12 +0800

    -

--- script.js
+++ script.js
@@ -12,13 +12,11 @@ const Myself=document.getElementById('Myself')
 
 function AboutMe(){
     for(i=1;i<=Me.length;i++){
-        if(Me[i-1].style.display==='none'){
-            Me[i-1].style.display='block'
-            achievements.style.animation='Achievements 1s linear 1'
-            Myself.style.animation='Myself 1s linear 1'
+        if(Me[i-1].style.visibility==='hidden'){
+            Me[i-1].style.visibility='visible'
         }
         else{
-            Me[i-1].style.display='none'
+            Me[i-1].style.visibility='hidden'
         }
     }
 }
