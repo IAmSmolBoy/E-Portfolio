da649229145a85611b2ab73281bb266fa4e20506
IAmSmlBoy <iamsmolboy>
2020-06-08 21:58:54 +0800

    -

--- script.js
+++ script.js
@@ -14,6 +14,9 @@ function AboutMe(){
     for(i=1;i<=Me.length;i++){
         if(Me[i-1].style.visibility==='hidden'){
             Me[i-1].style.visibility='visible'
+            Me[i-1].style.animationTimingFunction='linear'
+            Me[i-1].style.animationDuration='1s'
+            Me[i-1].style.animationName='SizeUp'
         }
         else{
             Me[i-1].style.visibility='hidden'
